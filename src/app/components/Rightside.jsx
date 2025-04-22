5cae8709082330a4f4a818348e49deb13ae273a4
Jesper <jefy0001@stud.kea.dk>
2025-04-22 13:02:42 +0200

    Add Rightside component to the application

---
+++ src/app/components/Rightside.jsx
@@ -0,0 +1,11 @@
+import Box1 from "../components/Box1";
+import Rightside from "../components/Rightside";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-screen">
+      <Box1 />
+      <Rightside />
+    </div>
+  );
+}
